f691d104023c5f13bbd69cad32ce88b44c6c2b35
Mark Ryan <mark@markryan.email>
2020-12-08 15:06:42 -0500

    Working part 1 of day 7

--- day 7/solution.r
+++ day 7/solution.r
@@ -11,18 +11,37 @@ data <- read_file("input.txt") %>%
 
 bags_allowed <- data %>%
   unnest(X2) %>%
-  mutate(X2 = str_replace(X2, "no other bags", "0 no other bags")) %>%
+  mutate(X1 = str_replace(X1, "bags", "")) %>%
+  mutate(X1 = str_trim(X1)) %>%
+  mutate(X2 = str_replace(X2, "no other bags", "0 NA")) %>%
+  mutate(X2 = str_replace(X2, "bags", "")) %>%
+  mutate(X2 = str_replace(X2, "bag", "")) %>%
   mutate(X2 = str_trim(X2)) %>%
   separate(X2, c("count", "inside_bag"), sep = "(?<=\\d) ")
 
 find_bags <- function(bag_type) {
   step_results <- bags_allowed %>% filter(inside_bag == bag_type)
-  for(i in 1:nrow(step_results)) {
-    
+  if(count(step_results) > 0 ){
+    for(i in 1:nrow(step_results)) {
+      parent <- step_results[i,]
+      if(count(parent) > 0 && as.numeric(parent$count) != 0) {
+        parent_bag <- find_bags(parent$X1)
+        if(!is.null(parent_bag)) {
+          step_results <- step_results %>%
+           add_row(find_bags(parent$X1))
+        }
+      }
+    }
+    return(step_results)
   }
+  return()
   
 }
-  
-  
+
+# Part 1
+# This takes forever to complete, obviously.
+res <- find_bags("shiny gold")
+print(length(unique(res$X1)))
+
   
  
